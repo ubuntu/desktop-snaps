2774436c0a6644520c68c7af6f7c31929cde527b
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2023-05-08 10:34:20 +0200

    Check for missing global variables
    The previous code only checked for functions, but not for global
    variables, since they are in a different symbol group.
    
    This patch adds checks for missing global variables.

--- abi-breaks/tests/lib1.c
+++ abi-breaks/tests/lib1.c
@@ -2,6 +2,8 @@ int function1(int a, char *b) {
     return a+(*b);
 }
 
+char variable_one;
+
 int function2(int a) {
     return a+1;
 }
--- abi-breaks/tests/lib3.c
+++ abi-breaks/tests/lib3.c
@@ -1,3 +1,5 @@
+char variable_one;
+
 int function2(int a) {
     return a+1;
 }
